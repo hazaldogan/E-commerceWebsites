9218651f5d90612a9c6072b6aa3a3d3525e289e9
Hazal Taştan <hazl.dogann@gmail.com>
2023-11-21 16:20:00 +0300

    Footer added

--- e-commerce/src/App.js
+++ e-commerce/src/App.js
@@ -1,12 +1,14 @@
 import Home from "./pages/Home";
 import Header from "./layouts/Header";
 import "./App.css";
+import Footer from "./layouts/Footer";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Home />
+      <Footer />
     </div>
   );
 }
