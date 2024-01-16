ff705de029424c8592199ff6dee41ee0fb553edd
Hazal Taştan <hazl.dogann@gmail.com>
2024-01-16 16:42:59 +0300

    shopping cart actions and reducers add

---
+++ e-commerce/src/hooks/useLocalStorage.js
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+function useLocalStorage(key, initialValue) {
+  const readFromLS = () => {
+    return JSON.parse(localStorage.getItem(key));
+  };
+
+  const [value, setValue] = useState(() => {
+    const lsOrDie = readFromLS() !== null ? readFromLS() : initialValue;
+
+    localStorage.setItem(key, JSON.stringify(lsOrDie));
+
+    return lsOrDie;
+  });
+
+  const writeToLSandState = (newValue) => {
+    setValue(newValue);
+    localStorage.setItem(key, JSON.stringify(newValue));
+  };
+
+  return [value, writeToLSandState];
+}
+
+export default useLocalStorage;
--- e-commerce/src/store/reducers/index.js
+++ e-commerce/src/store/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from "redux";
 import { globalReducer } from "./globalReducer";
 import { userReducer } from "./userReducers";
 import { productReducer } from "./productReducer";
+import { shopCartReducer } from "./shopCartReducer";
 
 export const reducers = combineReducers({
   productReducer,
   userReducer,
   globalReducer,
+  shopCartReducer,
 });
